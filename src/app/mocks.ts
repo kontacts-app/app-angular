6a55bbc1e66908a16c54d41d1ad8e0d6032ef397
Răzvan Roșu <razvanred99@gmail.com>
2024-05-16 17:03:23 +0200

    Update mocks

--- src/app/mocks.ts
+++ src/app/mocks.ts
@@ -5,9 +5,7 @@ export let mockContacts = [
     new Contact({ name: "Sara" }),
     new Contact({ name: "Dario" }),
     new Contact({ name: "Daniele" }),
-    new Contact({ name: "Matteo" }),
     new Contact({ name: "Marco" }),
     new Contact({ name: "Gianni" }),
     new Contact({ name: "Francesco" }),
-    new Contact({ name: "Stefano" }),
 ];
